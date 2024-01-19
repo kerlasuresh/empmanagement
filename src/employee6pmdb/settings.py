ca0a1d2d42589724ed60d12773630c9092569b33
unknown <suresh.k@vitelglobal.com>
2024-01-19 08:36:32 +0530

    pswd added:

--- src/employee6pmdb/settings.py
+++ src/employee6pmdb/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         "HOST": "localhost",
         "NAME": "emp_management",
         "USER": "postgres",
-        "PASSWORD": "postgres",
+        "PASSWORD": "Suri@9502",
         "PORT": 5432,
     }
 }
